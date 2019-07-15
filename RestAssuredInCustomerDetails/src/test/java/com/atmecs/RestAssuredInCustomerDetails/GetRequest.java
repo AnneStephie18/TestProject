c40353574d9315830fa04ec2875122ff047fdb70
anne.sivakumar <anne.sivakumar@atmecs.com>
2019-07-15 17:39:09 +0530

    automate crud services as get request in customer details use case

--- RestAssuredInCustomerDetails/src/test/java/com/atmecs/RestAssuredInCustomerDetails/GetRequest.java
+++ RestAssuredInCustomerDetails/src/test/java/com/atmecs/RestAssuredInCustomerDetails/GetRequest.java
@@ -1,11 +1,14 @@
 package com.atmecs.RestAssuredInCustomerDetails;
 
 import org.testng.annotations.Test;
-
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import junit.framework.Assert;
+/**
+ * automate the get request using rest assured 
+ * @author Anne.Sivakumar
+ *
+ */
 public class GetRequest {
 	@Test
 	public void GetService()
